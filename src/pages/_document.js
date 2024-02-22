9224c5c423c72c9a8eeca15783d571948eb327b0
Samuel Bankole <bankolesamuel175@gmail.com>
2024-02-22 01:41:19 +0100

    chore: deployment

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window,document,"clarity","script","l4p23gwddf");
+            })(window,document,"clarity","script","l4p0vfbhk0");
            `,
                 }}/>
         </Head>
