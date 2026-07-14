1faf268a6fc3baa83de30c5e3aed3b9a20d59949
DavidAdewale <adewaledavidola@gmail.com>
2026-07-14 16:50:11 +0100

    Re-enable sitewide redirect

--- next.config.js
+++ next.config.js
@@ -5,16 +5,15 @@ const nextConfig = {
     domains: ['moneda-treasury.000webhostapp.com'],
     unoptimized: true,
   },
-  // Re-enable this redirect when the old website should point all traffic to moneda.africa.
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       destination: 'https://moneda.africa',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://moneda.africa',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
